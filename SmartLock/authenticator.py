079546c9d128147e8897a37fb52638320c3dfcd7
Sandoval-Vargas Adrian (uia92823) <uia92823@anl933gu.conti.de>
2020-01-16 11:19:25 -0500

    added comments

--- SmartLock/authenticator.py
+++ SmartLock/authenticator.py
@@ -1,8 +1,10 @@
 import os, threading, webbrowser, subprocess
 from flask import Flask, render_template, request, flash, Blueprint
 
+#sets up the authenticator blueprint
 auth = Blueprint('auth', __name__)
 
+#route for the login 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return 'Log In'
--- SmartLock/web_server.py
+++ SmartLock/web_server.py
@@ -3,16 +3,15 @@ from flask import Flask, render_template, request, flash
 from authenticator import auth
 #Creates the application module to run upon running script -Adrian
 app = Flask(__name__)
-app.register_blueprint(auth)
+app.register_blueprint(auth) #registers the authenticator auth routing to this module - Adrian
 
-#This Route is the index page (landing page)
+#This Route is the index page (landing page) -Adrian
 @app.route('/', methods=['GET'])
 def index(): 
     return render_template('index.html')
    
 
-
-#Main function that executes the application
+#Main function that executes the application -Adrian
 if __name__ == '__main__':
     #runs the flask application using an IP Address, Debug set to true so test the site and modify on the fly
     app.run(debug=True)
