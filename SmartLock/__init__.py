eef5f248ac72fc42affbf88e8ef7763960ee8e30
Adrian <adriansandovalvargas@gmail.com>
2020-02-15 13:49:07 -0500

    test deploy

--- SmartLock/__init__.py
+++ SmartLock/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager as manager
 ##Creates db -Adrian
 db = SQLAlchemy()
 try:
+    app = create_app()
     ##Creates the Flask Application with the configurations -Adrian
     def create_app():
 
