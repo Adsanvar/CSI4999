96fa0d9214b87a79918ef630ab05af2c57ce24f5
sae442 <eid@oakland.edu>
2020-02-28 18:32:29 -0500

    keypad updates

--- keypad1/keypad/keypadrun.py
+++ keypad1/keypad/keypadrun.py
@@ -1,16 +1,14 @@
 import os, threading, webbrowser, subprocess
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from sqlalchemy.ext.automap import automap_base
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123eid123@localhost/smart_lock'
 
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
 
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column('id', db.Integer, primary_key=True)
-    pincode = db.Column('pincode', db.String(45))
 def openWeb():
     port = 4444
     url = "http://127.0.0.1:"+str(port)
@@ -22,9 +20,8 @@ def index():
     if request.method == 'POST':
         pin=request.form['code']
         print(pin)
-        examples = User.query.all()
-        #for ex in examples:
-            #print(ex.data)
+        
+        
         
 
 
