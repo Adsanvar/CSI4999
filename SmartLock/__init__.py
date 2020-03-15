ae404b751de988fbbab1dafcf56b8d4095f0770a
Adrian <adriansandovalvargas@gmail.com>
2020-03-15 14:28:46 -0400

    working on integration

--- SmartLock/__init__.py
+++ SmartLock/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager as manager
 
 ##Creates db -Adrian
 db = SQLAlchemy()
@@ -12,28 +11,17 @@ def create_app():
     #allows us to use Login Manager and other tools suchas Flash from flask_login - Adrian
     app.config['SECRET_KEY'] = 'test_secret_key'
     #this configures the databse for communication with flask -jared
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:pass@localhost/smart_lock'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:pass@localhost/smart_lock'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Database13.@localhost/rpi'
 
     # (return app) -Adrian
     db.init_app(app)
-
-    #initialized the login manager - Adrian
-    LoginManager = manager()
-    LoginManager.login_view = 'auth.login'
-    LoginManager.init_app(app)
-
+    
     #blueprints for the pages and models - Adrian
     from SmartLock.authenticator import auth as a_bp
     from SmartLock.web_server import home as h_bp
 
     app.register_blueprint(a_bp)
     app.register_blueprint(h_bp)
-
-    #used to query load the logged in user
-    from SmartLock.database import user_id_query as id_query
-
-    @LoginManager.user_loader
-    def load_user(id):
-        return id_query(id)
 
     return app
