ed00b000f84b37dc11338a541e54282551c4d623
Adrian <adriansandovalvargas@gmail.com>
2020-03-16 03:38:52 -0400

    working on integration

--- SmartLock/controller.py
+++ SmartLock/controller.py
@@ -1,13 +1,14 @@
 from gpiozero import LED
 from time import sleep
 
+
+gpio = LED(17)
+
 def GPIOon():
-    gpio = LED(17)
     gpio.on()
     sleep(10)
     GPIOoff()
 
 
 def GPIOoff():
-    gpio = LED(17)
     gpio.off()
