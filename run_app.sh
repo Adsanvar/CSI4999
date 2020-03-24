874a3130e4ea7b20b7e3a705bec360a649b0377d
Adrian <adriansandovalvargas@gmail.com>
2020-03-24 14:46:33 -0400

    Script update

--- run_app.sh
+++ run_app.sh
@@ -6,18 +6,17 @@ RpiIp=$(hostname -I)
 stringarray=($RpiIp)
 ip=${stringarray[0]}
 
-# datum=`python test.py "$ip"`
+python test.py "$ip"
 
-datum=$(python test.py "$ip" 2>&1 > /dev/null)
+# datum=$(python test.py "$ip" 2>&1 > /dev/null)
 
 
 #Eddystone protocol
-#sudo hcitool -i hci0 cmd 0x08 0x0008 1a 02 01 06 03 03 aa fe 12 16 aa fe 10 00 02 31 37 32 2e 32 30 2e 31 30 2e 33 3a 00 00 00 00 00
-sudo hcitool -i hci0 cmd $datum
-#frequency of advertisements
-sudo hcitool -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00 
-#some with eddystone
-sudo hcitool -i hci0 cmd 0x08 0x000a 01
+# sudo hcitool -i hci0 cmd 0x08 0x0008 1a 02 01 06 03 03 aa fe 12 16 aa fe 10 00 02 31 37 32 2e 32 30 2e 31 30 2e 33 3a 00 00 00 00 00
+# #frequency of advertisements
+# sudo hcitool -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00 
+# #some with eddystone
+# sudo hcitool -i hci0 cmd 0x08 0x000a 01
 
 export FLASK_APP=~/Documents/CSI4999/SmartLock
 export FLASK_ENV=development
