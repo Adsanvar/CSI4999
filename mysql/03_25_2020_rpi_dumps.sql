887fe2b6368c91b87552c8bd1cd0feb4736ae0a2
Adrian <adriansandovalvargas@gmail.com>
2020-03-25 11:57:24 -0400

    database

--- mysql/03_25_2020_rpi_dumps.sql
+++ mysql/03_25_2020_rpi_dumps.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE  IF NOT EXISTS `rpi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `rpi`;
 -- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
 --
--- Host: 127.0.0.1    Database: rpi
+-- Host: 127.0.0.1    Database: smart_lock
 -- ------------------------------------------------------
 -- Server version	8.0.13
 
