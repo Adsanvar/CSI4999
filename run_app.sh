0c1eebb8d101e759d7136aa22d9b822ddff2d9d1
Adrian <adriansandovalvargas@gmail.com>
2020-03-25 12:09:39 -0400

    find initialization file

--- run_app.sh
+++ run_app.sh
@@ -6,7 +6,7 @@ RpiIp=$(hostname -I)
 stringarray=($RpiIp)
 ip=${stringarray[0]}
 
-python3 initialization.py "$ip"
+python3 ~/Documents/CSI4999/initialization.py "$ip"
 
 # datum=$(python test.py "$ip" 2>&1 > /dev/null)
 
