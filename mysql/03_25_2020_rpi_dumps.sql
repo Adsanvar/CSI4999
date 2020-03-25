76f17650cffe49abf575cd5c707e717eaef10ea3
Adrian <adriansandovalvargas@gmail.com>
2020-03-25 11:54:06 -0400

    database

--- mysql/03_25_2020_rpi_dumps.sql
+++ mysql/03_25_2020_rpi_dumps.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `rpi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE  IF NOT EXISTS `rpi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `rpi`;
 -- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
 --
