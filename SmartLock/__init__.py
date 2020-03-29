bb366533e211435a10509fc92f2829cf740da2e4
Adrian <adriansandovalvargas@gmail.com>
2020-03-29 13:10:24 -0400

    door opening

--- SmartLock/__init__.py
+++ SmartLock/__init__.py
@@ -31,7 +31,7 @@ try:
         app.register_blueprint(h_bp)
 
         ip = os.getenv('IP_VAR')
-        threading.Timer(15, os.system('chromium-browser --start-fullscreen --kiosk http://'+ip+':5000')).start()
+        threading.Timer(30, os.system('chromium-browser --start-fullscreen --kiosk http://'+ip+':5000')).start()
 
         return app
 
